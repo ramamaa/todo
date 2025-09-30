ab7742efe292445b3aa88e6914858c61e390deec
Amar Bodonguud <64552518+ramamaa@users.noreply.github.com>
2025-09-30 20:31:36 +0800

    little bit css

--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -29,21 +29,35 @@ export default function Home() {
 
   return (
     <div className="m-8">
-      <div className="flex">
-        <input
-          className="input mr-4"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-        ></input>
-        <button className="btn btn-accent " onClick={createNewTask}>
-          Add
-        </button>
-      </div>
-      {tasks.map((task) => (
-        <div className="card p-4 border border-base-300 mt-4" key={task.id}>
-          {task.name}
+      <div className="max-w-[377px] items-center mx-auto py-6 px-4 rounded-md mt-15 shadow-lg">
+        <div className="flex flex-col gap-5">
+          <h1 className="leading-7 font-semibold text-xl flex justify-center">
+            To-Do list
+          </h1>
+          <div className="flex gap-1.5">
+            <input
+              placeholder="Add a new task..."
+              className="input hover:border-[#2463EB] hover:border-2 "
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+            />
+            <button
+              className="btn border rounded-md px-4 py-2 bg-[#3C82F6] text-white font-normal text-sm leading-[100%]"
+              onClick={createNewTask}>
+              Add
+            </button>
+          </div>
         </div>
-      ))}
+        {tasks.map((task) => (
+          <div className="card p-4 border border-base-300 mt-4" key={task.id}>
+            {task.name}
+          </div>
+        ))}
+        <h2 className="mt-10 flex gap-1 font-normal text-xs justify-center">
+          <span className="text-[#6B7280]">Powered by</span>
+          <span className="text-[#3B73ED]">Pinecone academy</span>
+        </h2>
+      </div>
     </div>
   );
 }
