cc05df94376e4fbdfb61a381d703de34ee7f41c8
Amar Bodonguud <64552518+ramamaa@users.noreply.github.com>
2025-10-03 12:33:20 +0800

    filter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,9 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import fs from "node:fs";
 
 const app: Application = express();
 const port = 4000;
-
-let tasks = [
-  { id: "1", name: "shalaa ugaah" },
-  { id: "2", name: "ayagaa ugaah" },
-];
 
 app.use(cors());
 app.use(express.json());
@@ -15,29 +11,61 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+function getTasks() {
+  const data = fs.readFileSync("data.text", "utf8");
+  const tasks = JSON.parse(data);
+  return tasks;
+}
+
+function writeTasks(tasks: { id: string; name: string }[]) {
+  fs.writeFile("data.text", JSON.stringify(tasks), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
 app.get("/tasks", (req: Request, res: Response) => {
+  const tasks = getTasks();
   res.send(tasks);
 });
+
 app.post("/tasks", (req: Request, res: Response) => {
+  const tasks = getTasks();
   const id = new Date().getMilliseconds().toString();
   const { name } = req.body;
   tasks.unshift({ id: id, name });
+
+  writeTasks(tasks);
+
   res.status(201).send("Created");
 });
 app.delete("/tasks/:id", (req: Request, res: Response) => {
   const id = req.params.id;
-  const newTask = tasks.filter((task) => task.id !== id);
-  tasks = newTask;
+  const tasks = getTasks();
 
+  const newTask = tasks.filter((task: { id: string }) => task.id !== id);
+  writeTasks(newTask);
   res.sendStatus(204); // No Content
 });
 
 app.put("/tasks/:id", (req: Request, res: Response) => {
   const id = req.params.id;
   const { name } = req.body;
-  const index = tasks.findIndex((tasks) => tasks.id === id);
+  const tasks = getTasks();
+  const index = tasks.findIndex((tasks: { id: string }) => tasks.id === id);
   tasks[index].name = name;
+  writeTasks(tasks);
+  res.sendStatus(204);
+});
 
+app.patch("/tasks/:id/check", (req: Request, res: Response) => {
+  const id = req.params.id;
+  const tasks = getTasks();
+  const index = tasks.findIndex((task: { id: string }) => task.id === id);
+  tasks[index].isCompleted = !tasks[index].isCompleted;
+  writeTasks(tasks);
   res.sendStatus(204);
 });
 
--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useEffect, useState } from "react";
-
+type Task = { id: string; name: string; isCompleted: boolean };
 export default function Home() {
   const [newTask, setNewTask] = useState("");
-  const [tasks, setTasks] = useState<{ id: string; name: string }[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [status, setStatus] = useState("All");
+
+  const [editingId, setEditingId] = useState("");
 
   async function createNewTask() {
     if (newTask) {
@@ -26,19 +29,26 @@ export default function Home() {
       });
   }
 
-  async function editTask(task: { id: string; name: string }) {
-    const newName = prompt("Edit", task.name);
-    if (newName) {
-      await fetch(`http://localhost:4000/tasks/${task.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name: newName }),
-      });
+  async function editTask(task: Task) {
+    setEditingId(task.id);
+    // const newName = prompt("Edit", task.name);
+    // if (newName) {
+    //   await fetch(`http://localhost:4000/tasks/${task.id}`, {
+    //     method: "PUT",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ name: newName }),
+    //   });
 
-      loadTasks();
-    }
+    //   loadTasks();
+    // }
+  }
+
+  async function toggleCompleted(id: string) {
+    await fetch(`http://localhost:4000/tasks/${id}/check`, {
+      method: "PATCH",
+    });
   }
 
   async function deleteTask(id: string) {
@@ -77,26 +87,58 @@ export default function Home() {
               Add
             </button>
           </div>
+
+          <div role="tablist" className="tabs tabs-box mt-5">
+            {["All", "Active", "Completed"].map((s) => (
+              <a
+                key={s}
+                role="tab"
+                onClick={() => setStatus(s)}
+                className={`tab flex-1 ${status === s ? "tab-active" : ""}`}
+              >
+                {s}
+              </a>
+            ))}
+          </div>
+
           {tasks.map((task) => (
             <div
               className="card p-4 border border-base-300 mt-4 bg-[#F9FAFB]"
               key={task.id}
             >
-              <div className="flex items-center gap-1">
-                <div className="flex-1">{task.name}</div>
-                <button
+              {editingId === task.id ? (
+                <div className="flex gap-3">
+                  <input defaultValue={task.name} />
+                  <button>Done</button>
+                </div>
+              ) : (
+                <div
                   onClick={() => editTask(task)}
-                  className="btn btn-xs btn-ghost"
+                  className="flex items-center gap-1 cursor-pointer"
                 >
-                  Edit
-                </button>
-                <button
-                  className="bg-[#FEF2F2] text-[#EF4444] text-sm font-normal rounded-md py-1.5 px-3"
-                  onClick={() => deleteTask(task.id)}
-                >
-                  Delete
-                </button>
-              </div>
+                  <input
+                    onChange={() => {
+                      toggleCompleted(task.id);
+                    }}
+                    defaultChecked={task.isCompleted}
+                    type="checkbox"
+                    className="checkbox checkbox-info"
+                  />
+                  <div className="flex-1">{task.name}</div>
+                  {/* <button
+                    onClick={() => editTask(task)}
+                    className="btn btn-xs btn-ghost"
+                  >
+                    Edit
+                  </button> */}
+                  <button
+                    className="bg-[#FEF2F2] text-[#EF4444] text-sm font-normal rounded-md py-1.5 px-3"
+                    onClick={() => deleteTask(task.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              )}
             </div>
           ))}
         </div>
